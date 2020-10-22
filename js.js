4e43266c0c3bc2fe32b4a54acc2ec7f2156097ee
AtteLoponen <58698919+AtteLoponen@users.noreply.github.com>
2020-10-22 17:42:49 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,51 @@
+// Luodaan sulje nappula
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u03A7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+// Listan aihe yliviivataan
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+// Luodaan uusi listan aihe lisää nappia painettaessa
+function function1() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("input").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("Sinun täytyy kirjoittaa jotain!");
+    } else {
+        document.getElementById("ul").appendChild(li);
+    }
+    document.getElementById("input").value = "";
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u03A7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
